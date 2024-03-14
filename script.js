d2ca43135fa40de5039b0dd80933d922f2378b44
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-14 21:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let level = document.getElementById('level');
+const root = document.documentElement;
+let count = 1;
+while(level!==root){
+	count ++;
+	level = level.parentElement;
+}
+alert(`The level of the element is: ${count}`);
